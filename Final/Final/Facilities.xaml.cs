be3b07fc0418a17a2fdfdaf2318836beb36cf3fa
TysonHEdu <tyson.harder@edu.sait.ca>
2023-04-26 14:48:23 -0600

    0.5
    Front end basically done

--- Final/Final/Facilities.xaml.cs
+++ Final/Final/Facilities.xaml.cs
@@ -1,9 +1,17 @@
+using System.Diagnostics.Metrics;
+
 namespace Final;
 
 public partial class Facilities : ContentPage
 {
+	int count = 0;
 	public Facilities()
 	{
 		InitializeComponent();
 	}
+
+    private void OrderButton_Clicked(object sender, EventArgs e)
+    {
+		DisplayAlert("1", "Has Been Ordered", "Close");
+    }
 }
